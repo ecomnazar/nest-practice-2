12cb3a6d25d59c961031b549a1cc88a777349bab
ecomnazar <ecomnazar.me@gmail.com>
2024-02-13 15:56:24 +0500

    [completed]: crud

--- src/entities/user/user.controller.ts
+++ src/entities/user/user.controller.ts
@@ -3,25 +3,34 @@ import {
   Get,
   Req,
   Res,
+  Param,
   Post,
   Put,
-  Patch,
   Delete,
-  UseInterceptors,
+  // UseInterceptors,
+  ParseIntPipe,
+  Body,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+// import { FileInterceptor } from '@nestjs/platform-express';
 import { Request, Response } from 'express';
 import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/updateUser.dto';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('/')
-  async getAllUsers(@Req() req: Request, @Res() res: Response) {}
+  async getAllUsers(@Req() req: Request, @Res() res: Response) {
+    const users = await this.userService.getAllUsers();
+    res.send({ status: 'ok', data: users });
+  }
 
   @Get('/:id')
-  async getUser(@Req() req: Request, @Res() res: Response) {}
+  async getUser(@Param('id', ParseIntPipe) id: number, @Res() res: Response) {
+    const userData = await this.userService.getUser(id);
+    res.send({ status: 'ok', data: userData });
+  }
 
   @Post('/')
   //   @UseInterceptors(FileInterceptor(''))
@@ -31,11 +40,21 @@ export class UserController {
   }
 
   @Put('/:id')
-  async updateUser(@Req() req: Request, @Res() res: Response) {}
-
-  @Patch('/:id')
-  async updateUserField(@Req() req: Request, @Res() res: Response) {}
+  async updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: UpdateUserDto,
+    @Res() res: Response,
+  ) {
+    await this.userService.updateUserData(id, body);
+    res.send({ status: 'ok' });
+  }
 
   @Delete('/:id')
-  async deleteUser(@Req() req: Request, @Res() res: Response) {}
+  async deleteUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Res() res: Response,
+  ) {
+    await this.userService.deleteUser(id);
+    res.send({ status: 'ok' });
+  }
 }
--- src/entities/user/user.service.ts
+++ src/entities/user/user.service.ts
@@ -1,15 +1,31 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { genSalt, hash } from 'bcrypt';
-
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { UpdateUserDto } from './dto/updateUser.dto';
+
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
+  availabelFields = ['nameFirst', 'nameLast', 'email', 'gender', 'birthDate'];
+
+  // filter body's fields from available fields list
+  private filterFields(body: { [k: string]: any }) {
+    const filteredBody: { [k: string]: any } = {};
+
+    Object.keys(body).filter((k) => {
+      if (this.availabelFields.includes(k)) {
+        filteredBody[k] = body[k];
+      }
+    });
+    return filteredBody;
+  }
+
+  // add user
   public async createUser(userData: any) {
     const salt = await genSalt(10);
     const hashedPassword = await hash(userData.password, salt);
@@ -19,5 +35,31 @@ export class UserService {
       password: hashedPassword,
     });
     return await this.userRepository.save(user);
+  }
+
+  // get all users
+  public async getAllUsers() {
+    return await this.userRepository.find({
+      select: this.availabelFields as any,
+    });
+  }
+
+  // get user
+  public async getUser(id: number) {
+    return await this.userRepository.findOne({
+      where: { id },
+      select: ['birthDate', 'email', 'gender', 'id', 'nameFirst', 'nameLast'],
+    });
+  }
+
+  // update user data whole
+  public async updateUserData(id: number, body: UpdateUserDto) {
+    // const { nameFirst, nameLast, email, gender, birthDate } = body;
+    return await this.userRepository.update({ id }, this.filterFields(body));
+  }
+
+  // delete user by id
+  public async deleteUser(id: number) {
+    return await this.userRepository.delete(id);
   }
 }
